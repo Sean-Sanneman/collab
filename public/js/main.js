3234d0bbd863fa01b570dbbd094e898b38827efd
Jason Ahmed <jason_ahmed@yahoo.com>
2020-08-31 15:26:58 -0700

    added pull-down menus and reformatted grids

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,30 @@
 $(document).ready(function(){
     $('.header').height($(window).height());
+
+// Dropdown menus event-handler:
+
+// NEED TO FIX THE ON-CHANGE - ASK CENAN
+function myInstrumentDropdown() {
+  var x = document.getElementById("myInstrument").value;
+  console.log(x)
+
+  // ADD FETCH CALL TO DATABASE HERE! //
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   })
+
